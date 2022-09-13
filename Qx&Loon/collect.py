91a301bc9e55bc7f108fc9e12b106e600bb232fc
zhousanfu <zhousanfukig@icloud.com>
2022-09-13 18:23:52 +0800

    移动路径

---
+++ Qx&Loon/collect.py
@@ -0,0 +1,9 @@
+#!python3.8
+# coding=utf-8
+'''
+ Author: Sanfor Chow
+ Date: 2022-09-13 18:22:30
+ LastEditors: Sanfor Chow
+ LastEditTime: 2022-09-13 18:22:31
+ FilePath: /rules_script/Qx&Loon/collect.py
+'''
